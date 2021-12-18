f5f8ef813b12fffaf6a3516d31c4561559bac8b3
Vitaliy Gutnitskiy <vitgutnitskiy@gmail.com>
2021-12-19 00:03:25 +0200

    validParentheses

---
+++ js/validParentheses.js
@@ -0,0 +1,12 @@
+function validParentheses(parens) {
+  const arrayParens = parens.split("");
+  let s = 0;
+  for (let i = 0; i < parens.length; i++) {
+    if (arrayParens[i] === "(") s = s + 1;
+    else if (arrayParens[i] === ")") s = s - 1;
+    if (s < 0) return false;
+  }
+  if (s === 0) return true;
+
+  return false;
+}
