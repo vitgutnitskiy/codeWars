5605b0fdf662a5397b645accae647526d2e220f5
Vitaliy Gutnitskiy <vitgutnitskiy@gmail.com>
2021-12-21 09:05:33 +0200

    Update lastDigit.js

--- js/lastDigit.js
+++ js/lastDigit.js
@@ -1,3 +1,10 @@
 var lastDigit = function (str1, str2) {
   return +!+str2 || Math.pow(str1.slice(-1), str2.slice(-2) % 4 || 4) % 10;
 };
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
